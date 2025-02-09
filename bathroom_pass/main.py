4d77dfc5b5aed3424cf6b9da62362f16c222fbbe
M8tth3 <mw.mountaindragon@gmail.com>
2025-02-08 22:23:34 -0800

    scanner error handling

--- bathroom_pass/main.py
+++ bathroom_pass/main.py
@@ -13,72 +13,45 @@ queue = ["John Mortensen", "Srijan Atti"]
 current_user = ""
 next_up = "Matthew Wakayama"
 def barcode_reader():
-    """Barcode code obtained from 'brechmos' 
-    https://www.raspberrypi.org/forums/viewtopic.php?f=45&t=55100"""
     hid = {4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm',
            17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y',
-           29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ',
-           45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';', 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'}
+           29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' '}
 
     hid2 = {4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G', 11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M',
             17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23: 'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y',
-            29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ',
-            45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':', 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'}
+            29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' '}
 
     fp = open('/dev/hidraw4', 'rb')
 
     ss = ""
     shift = False
 
-    done = False
-
-    while not done:
-
-        ## Get the character from the HID
+    while True:
         buffer = fp.read(8)
         for c in buffer:
-            if ord(c) > 0:
+            keycode = int(c)  # Convert byte to integer
+            print(f"Keycode received: {keycode}")  # Debugging output
 
-                ##  40 is carriage return which signifies
-                ##  we are done looking for characters
-                if int(ord(c)) == 40:
-                    done = True
-                    break;
+            if keycode == 40:  # Enter key (end of barcode)
+                print(f"Final scanned barcode: {ss}")
+                name = get_name_by_sid
+                add_to_queue("jmort1021@gmail.com", name, javaURI)
 
-                ##  If we are shifted then we have to
-                ##  use the hid2 characters.
-                if shift:
+                return ss  
 
-                    ## If it is a '2' then it is the shift key
-                    if int(ord(c)) == 2:
-                        shift = True
+            if keycode == 2:  # Shift key
+                shift = True
+                continue
 
-                    ## if not a 2 then lookup the mapping
-                    else:
-                        ss += hid2[int(ord(c))]
-                        shift = False
-
-                ##  If we are not shifted then use
-                ##  the hid characters
-
-                else:
-
-                    ## If it is a '2' then it is the shift key
-                    if int(ord(c)) == 2:
-                        shift = True
-
-                    ## if not a 2 then lookup the mapping
-                    else:
-                        ss += hid[int(ord(c))]
-
-    print("ss: ", ss)
-    name = get_name_by_sid(ss)
-
-    add_to_queue("jmort1021@gmail.com", name, javaURI)
-    return ss
-
-
-
+            # Handle unknown keycodes
+            if keycode in hid or keycode in hid2:
+                if shift and keycode in hid2:
+                    ss += hid2[keycode]
+                elif keycode in hid:
+                    ss += hid[keycode]
+                shift = False
+            else:
+                print(f"Warning: Unmapped keycode {keycode}")  # Debugging unknown keycodes
 
 
 def send_barcode_to_server(student_id):
