869fcd4c3e6844503d2157bfb866e23c67cac7d5
AidanLau10 <aidanlau10@gmail.com>
2025-02-12 19:17:12 -0800

    revert tanav and sri commit
    git revert --no-commit b4515bc0b7fed737e8fc2ac5e292d9463fc4a57f..6585b016efefaf687297f8f25aa82ec25c33c2cb

--- assets/js/adventureGame/Npc.js
+++ assets/js/adventureGame/Npc.js
@@ -1,9 +1,8 @@
 import GameEnv from "./GameEnv.js";
 import Character from "./Character.js";
 import gameControlInstance from "./GameControl.js";
-import MiniLevel from "./MiniLevel.js";
-import Prompt from "./Prompt.js";
-import npcTrackerInstance from "./NpcTracker.js"; // Import NPC Tracker
+import MiniLevel from "./MiniLevel.js"; 
+import Prompt from "./Prompt.js"; // Import the Prompt class
 
 class Npc extends Character {
     constructor(data = null) {
@@ -12,7 +11,7 @@ class Npc extends Character {
         this.questions = this.shuffleArray(data?.quiz?.questions || []); // Shuffle questions
         this.currentQuestionIndex = 0;
         this.alertTimeout = null;
-        this.prompt = new Prompt();
+        this.prompt = new Prompt(); // Create a new Prompt instance
         this.bindInteractKeyListeners();
     }
 
@@ -47,36 +46,42 @@ class Npc extends Character {
         const hasQuestions = this.questions.length > 0;
 
         if (players.length > 0) {
-            npcTrackerInstance.addNpc(this.spriteData.id); // Track NPC
-
+            // Case 1: If we are in a main level, move the player to the mini level and don't ask questions
             if (!(gameControlInstance.currentLevel instanceof MiniLevel)) {
                 if (hasQuestions) {
                     gameControlInstance.startMiniLevel(this);
+                    // Do not ask questions yet, only start the mini-level
                 }
-            } else if (gameControlInstance.currentLevel instanceof MiniLevel) {
-                this.prompt.openPromptPanel(this);
+            }
+            // Case 2: If we are in a mini level, use the Prompt class to ask the questions
+            else if (gameControlInstance.currentLevel instanceof MiniLevel) {
+                this.prompt.openPromptPanel(this); // Open the prompt panel with the questions
             }
         }
     }
 
     askNextQuestion() {
         if (this.currentQuestionIndex < this.questions.length) {
+            // Prompt the next question
             const question = this.questions[this.currentQuestionIndex];
             const playerAnswer = prompt(question);
             this.checkAnswer(playerAnswer);
         } else {
+            // Case 3: If all questions are finished in the mini level, move the player back to the main level
             this.completeQuiz();
         }
     }
 
     checkAnswer(answer) {
+        // You can implement your answer checking logic here
         alert(`You answered: ${answer}`);
         this.currentQuestionIndex++;
-        this.askNextQuestion();
+        this.askNextQuestion(); // Proceed to the next question
     }
 
     completeQuiz() {
         alert("Congratulations! You completed the quiz.");
+        // After finishing the mini-level, move to the next main level
         gameControlInstance.handleMiniLevelEnd();
     }
 
