15efc6942fde2ff783820a965942a8a6352db58a
jm1021 <jmort1021@gmail.com>
2025-02-17 19:57:18 -0800

    comments added for interact - game in game

--- assets/js/adventureGame/GameLevelWater.js
+++ assets/js/adventureGame/GameLevelWater.js
@@ -5,11 +5,12 @@ import GameControl from './GameControl.js';
 import GameLevelStarWars from './GameLevelStarWars.js';
 
 class GameLevelWater {
+  /**
+   * Properties and methods to define a game level
+   * @param {*} gameEnv - The active game environment
+   */
   constructor(gameEnv) {
-    const header = document.querySelector('header');
-    const footer = document.querySelector('footer');
-
-    // Values dependent on GameEnv.create()
+    // Dependencies to support game level creation
     let width = gameEnv.innerWidth;
     let height = gameEnv.innerHeight;
     let path = gameEnv.path;
@@ -23,6 +24,7 @@ class GameLevelWater {
         pixels: {height: 597, width: 340}
     };
 
+    // Player Data for Octopus
     const sprite_src_octopus = path + "/images/gamify/octopus.png"; // be sure to include the path
     const OCTOPUS_SCALE_FACTOR = 5;
     const sprite_data_octopus = {
@@ -36,7 +38,7 @@ class GameLevelWater {
         pixels: {height: 250, width: 167},
         orientation: {rows: 3, columns: 2 },
         down: {row: 0, start: 0, columns: 2 },
-        left: {row: 1, start: 0, columns: 2, mirror: true },
+        left: {row: 1, start: 0, columns: 2, mirror: true }, // mirror is used to flip the sprite
         right: {row: 1, start: 0, columns: 2 },
         up: {row: 0, start: 0, columns: 2},
         hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
@@ -56,22 +58,27 @@ class GameLevelWater {
         orientation: {rows: 3, columns: 7 },
         down: {row: 1, start: 0, columns: 6 },  // This is the stationary npc, down is default 
         hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
+        /* Interact function
+        *  This function is called when the player interacts with the NPC
+        *  It pauses the main game, creates a new GameControl instance with the StarWars level,
+        */
         interact: function() {
-          // Pause GameControl on main game .pause() method
+          // Call .pause to stop the underlying game 
           gameControl.pause();
-          let level = [GameLevelStarWars];
-          let gameInGame = new GameControl(path,level);
+          // Start the intreact requested game in game
+          let levelArray = [GameLevelStarWars];
+          let gameInGame = new GameControl(path,levelArray);
           gameInGame.start();
+          // Setup "callback" function to allow transition from game in gaame to the underlying game
           gameInGame.onLevelEnd = function() {
-            // Resume GameControl on main game .resume() method
+            // Call .resume underlying game
             gameControl.resume();
           }
         }
 
       };
 
-
-    // List of objects definitions for this level
+    // List of classes and supporting definitions to create the game level
     this.classes = [
       { class: Background, data: image_data_water },
       { class: Player, data: sprite_data_octopus },
