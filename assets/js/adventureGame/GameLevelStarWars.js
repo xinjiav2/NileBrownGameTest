fbe0842565cc26381ac34937e96f90aed9ac534b
jm1021 <jmort1021@gmail.com>
2025-02-19 22:15:21 -0800

    change sprite to be more like a laser ball

--- assets/js/adventureGame/GameLevelStarWars.js
+++ assets/js/adventureGame/GameLevelStarWars.js
@@ -45,18 +45,18 @@ class GameLevelStarWars {
         greeting: "Simulate explosive action!",
         // define image/sprite data
         src: sprite_src_bomb,
-        pixels: {height: 154, width: 525}, // height and width of the image
+        pixels: {height: 500, width: 500}, // height and width of the image
         orientation: {rows: 1, columns: 1 }, // normalized rows and columns in the sprite
         // define size, position, adjustments for hitbox
-        SCALE_FACTOR: 20,  // Start small 1/20 scale and grow
+        SCALE_FACTOR: 30,  // Start small 1/20 scale and grow
         INIT_POSITION_RATIO: { x: 1 / 1.78, y: 1 / 3.3 }, // Ratios for initial position
         hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
         // define animation properties
-        ANIMATION_RATE: 50, // 1/50th of the frame rate for redraw
+        ANIMATION_RATE: 100, // 1/50th of the frame rate for redraw
         TRANSLATE_SCALE_FACTOR: 10, // Grow to 1/10 scale at end of translation
         TRANSLATE_POSITION_RATIO: { x: 1 / 2.22, y: 1 / 2.7 }, // Ratios for translate position
         TRANSLATE_SIMULATION: {miliseconds: 1000, steps: 10}, // 1 second, 10 steps
-        down: {row: 0, start: 0, columns: 1},  // This is the stationary bomb, down is default
+        down: {row: 0, start: 0, columns: 1, rotate: Math.PI / 1.5},  // This is the stationary bomb, down is default
      };
 
     // List of objects definitions for this level
